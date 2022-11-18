b190c747f7d7761467b8944d8442389da2dbfe0d
Edgard <20133252@ealoe.ulima.edu.pe>
2022-11-18 08:42:10 -0500

    Initial Commit

---
+++ CD-CRUD-MVVM/CD_CRUD_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CD_CRUD_MVVMApp.swift
+//  CD-CRUD-MVVM
+//
+//  Created by EdgardVS on 18/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CD_CRUD_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
